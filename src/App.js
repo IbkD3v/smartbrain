e3b1df1753b54047c120bde4c39ee3be93b3655e
Ibukun <awopetuid@gmail.com>
2019-07-15 03:38:12 +0100

    Face Recognition

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ class App extends React.Component {
         this.state.input)
         .then(
           function(response){
-            console.log(response);
+            console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
           },
           function(err){
 
